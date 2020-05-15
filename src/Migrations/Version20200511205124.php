3225b73f1a134cb6b18430ccf18554b2e86e34e5
Borja Gonzalez <borja@bgon.es>
2020-05-15 13:19:39 +0200

    Migrations fix

--- src/Migrations/Version20200511205124.php
+++ src/Migrations/Version20200511205124.php
@@ -27,7 +27,7 @@ final class Version20200511205124 extends AbstractMigration
         $this->addSql('ALTER TABLE player CHANGE roles roles JSON NOT NULL, CHANGE twitch_name twitch_name VARCHAR(255) DEFAULT NULL, CHANGE twitch_id twitch_id VARCHAR(255) DEFAULT NULL');
         $this->addSql('ALTER TABLE registration CHANGE elo elo INT DEFAULT NULL, CHANGE is_sub is_sub TINYINT(1) DEFAULT NULL, CHANGE priority priority INT DEFAULT NULL, CHANGE is_confirmed is_confirmed TINYINT(1) DEFAULT NULL');
         $this->addSql('ALTER TABLE round CHANGE winner_id winner_id INT DEFAULT NULL, CHANGE lobby_name lobby_name VARCHAR(255) DEFAULT NULL, CHANGE lobby_password lobby_password VARCHAR(255) DEFAULT NULL');
-        $this->addSql('ALTER TABLE team CHANGE lobby_name lobby_name VARCHAR(255) DEFAULT NULL, CHANGE lobby_password lobby_password VARCHAR(255) DEFAULT NULL, CHANGE rank rank INT DEFAULT NULL, CHANGE name name VARCHAR(255) DEFAULT NULL');
+        $this->addSql('ALTER TABLE team CHANGE lobby_name lobby_name VARCHAR(255) DEFAULT NULL, CHANGE lobby_password lobby_password VARCHAR(255) DEFAULT NULL, CHANGE ranking ranking INT DEFAULT NULL, CHANGE name name VARCHAR(255) DEFAULT NULL');
     }
 
     public function down(Schema $schema) : void
@@ -40,6 +40,6 @@ final class Version20200511205124 extends AbstractMigration
         $this->addSql('ALTER TABLE player CHANGE roles roles LONGTEXT CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_bin`, CHANGE twitch_name twitch_name VARCHAR(255) CHARACTER SET utf8mb4 DEFAULT \'NULL\' COLLATE `utf8mb4_unicode_ci`, CHANGE twitch_id twitch_id VARCHAR(255) CHARACTER SET utf8mb4 DEFAULT \'NULL\' COLLATE `utf8mb4_unicode_ci`');
         $this->addSql('ALTER TABLE registration CHANGE elo elo INT DEFAULT NULL, CHANGE is_sub is_sub TINYINT(1) DEFAULT \'NULL\', CHANGE priority priority INT DEFAULT NULL, CHANGE is_confirmed is_confirmed TINYINT(1) DEFAULT \'NULL\'');
         $this->addSql('ALTER TABLE round CHANGE winner_id winner_id INT DEFAULT NULL, CHANGE lobby_name lobby_name VARCHAR(255) CHARACTER SET utf8mb4 DEFAULT \'NULL\' COLLATE `utf8mb4_unicode_ci`, CHANGE lobby_password lobby_password VARCHAR(255) CHARACTER SET utf8mb4 DEFAULT \'NULL\' COLLATE `utf8mb4_unicode_ci`');
-        $this->addSql('ALTER TABLE team CHANGE lobby_name lobby_name VARCHAR(255) CHARACTER SET utf8mb4 DEFAULT \'NULL\' COLLATE `utf8mb4_unicode_ci`, CHANGE lobby_password lobby_password VARCHAR(255) CHARACTER SET utf8mb4 DEFAULT \'NULL\' COLLATE `utf8mb4_unicode_ci`, CHANGE rank rank INT DEFAULT NULL, CHANGE name name VARCHAR(255) CHARACTER SET utf8mb4 DEFAULT \'NULL\' COLLATE `utf8mb4_unicode_ci`');
+        $this->addSql('ALTER TABLE team CHANGE lobby_name lobby_name VARCHAR(255) CHARACTER SET utf8mb4 DEFAULT \'NULL\' COLLATE `utf8mb4_unicode_ci`, CHANGE lobby_password lobby_password VARCHAR(255) CHARACTER SET utf8mb4 DEFAULT \'NULL\' COLLATE `utf8mb4_unicode_ci`, CHANGE ranking ranking INT DEFAULT NULL, CHANGE name name VARCHAR(255) CHARACTER SET utf8mb4 DEFAULT \'NULL\' COLLATE `utf8mb4_unicode_ci`');
     }
 }
