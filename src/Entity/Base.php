e5daf23adfec494f883c4d9caaea2d927e87f6f1
Borja Gonzalez <borja@bgon.es>
2020-06-01 16:11:45 +0200

    Player extends Base again with no error

--- src/Entity/Base.php
+++ src/Entity/Base.php
@@ -14,17 +14,17 @@ abstract class Base
      * @ORM\GeneratedValue()
      * @ORM\Column(type="integer")
      */
-    private $id;
+    protected $id;
 
     /**
      * @ORM\Column(type="datetime")
      */
-    private $createdAt;
+    protected $createdAt;
 
     /**
      * @ORM\Column(type="datetime")
      */
-    private $updatedAt;
+    protected $updatedAt;
 
     public function __construct()
     {
@@ -37,35 +37,14 @@ abstract class Base
         return $this->id;
     }
 
-    public function setId(int $id): self
-    {
-        $this->id = $id;
-
-        return $this;
-    }
-
     public function getCreatedAt(): ?\DateTimeInterface
     {
         return $this->createdAt;
     }
 
-    public function setCreatedAt(\DateTimeInterface $createdAt): self
-    {
-        $this->createdAt = $createdAt;
-
-        return $this;
-    }
-
     public function getUpdatedAt(): ?\DateTimeInterface
     {
         return $this->updatedAt;
-    }
-
-    public function setUpdatedAt(\DateTimeInterface $updatedAt): self
-    {
-        $this->updatedAt = $updatedAt;
-
-        return $this;
     }
 
     /** @ORM\PreUpdate() */
--- src/Entity/Player.php
+++ src/Entity/Player.php
@@ -11,25 +11,8 @@ use Symfony\Component\Validator\Constraints as Assert;
 /**
  * @ORM\Entity(repositoryClass="App\Repository\PlayerRepository")
  */
-class Player implements UserInterface
+class Player extends Base implements UserInterface
 {
-    /**
-     * @ORM\Id()
-     * @ORM\GeneratedValue()
-     * @ORM\Column(type="integer")
-     */
-    private $id;
-
-    /**
-     * @ORM\Column(type="datetime")
-     */
-    private $createdAt;
-
-    /**
-     * @ORM\Column(type="datetime")
-     */
-    private $updatedAt;
-
     /**
      * @ORM\Column(type="string", length=180, unique=true)
      */
@@ -89,60 +72,10 @@ class Player implements UserInterface
 
     public function __construct()
     {
-        $this->createdAt = new \DateTime();
-        $this->updatedAt = new \DateTime();
+        parent::__construct();
         $this->registrations = new ArrayCollection();
         $this->teams = new ArrayCollection();
         $this->competitionsCreated = new ArrayCollection();
-    }
-
-    public function getId(): ?int
-    {
-        return $this->id;
-    }
-
-    public function setId(int $id): self
-    {
-        $this->id = $id;
-
-        return $this;
-    }
-
-    public function getCreatedAt(): ?\DateTimeInterface
-    {
-        return $this->createdAt;
-    }
-
-    public function setCreatedAt(\DateTimeInterface $createdAt): self
-    {
-        $this->createdAt = $createdAt;
-
-        return $this;
-    }
-
-    public function getUpdatedAt(): ?\DateTimeInterface
-    {
-        return $this->updatedAt;
-    }
-
-    public function setUpdatedAt(\DateTimeInterface $updatedAt): self
-    {
-        $this->updatedAt = $updatedAt;
-
-        return $this;
-    }
-
-    /** @ORM\PreUpdate() */
-    protected function updateNow(): self
-    {
-        $this->setUpdatedAt = new \DateTime();
-
-        return $this;
-    }
-
-    public function equals(Base $object): bool
-    {
-        return get_class($object) === get_called_class() && $this->getId() === $object->getId();
     }
 
     /**
